be563bb7720c8006df1dddce038043bf659d3536
shimizu_s <shoji@shimizu-i.com>
2019-03-22 14:10:17 +0900

    modified:   init.el

--- init.el
+++ init.el
@@ -239,15 +239,15 @@
 ;;   (highlight-indent-guides-responsive t)
 ;;   (highlight-indent-guides-method 'character)) ; column
 
-(use-package yascroll
-  :ensure t
-  :after ivy
-  :custom-face
-  (yascroll:thumb-fringe ((t (:background "#565761" :foreground "#565761"))))
-  :config
-  (message ":config yascroll")
-  (global-yascroll-bar-mode t)
-  )
+;; (use-package yascroll
+;;   :ensure t
+;;   :after ivy
+;;   :custom-face
+;;   (yascroll:thumb-fringe ((t (:background "#565761" :foreground "#565761"))))
+;;   :config
+;;   (message ":config yascroll")
+;;   (global-yascroll-bar-mode t)
+;;   )
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; @ Interface
@@ -1073,3 +1073,17 @@
 
 ;; ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;; @ auto
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yascroll yasnippet writeroom-mode which-key volatile-highlights use-package smooth-scroll realgud rainbow-mode rainbow-delimiters pt nyan-mode neotree minimap lsp-ui ivy-rich imenu-list hydra hl-todo highlight-indent-guides hide-mode-line hemisu-theme helm-make gruvbox-theme git-gutter flycheck-posframe fill-column-indicator evil-magit evil-collection elisp-format doom-themes doom-modeline counsel company-box clang-format blacken beacon anzu all-the-icons-ivy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(realgud-bp-line-enabled-face ((t (:underline "red")))))
