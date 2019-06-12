2f8cfbcbe61b3fc6cc549f4cffe67eb6c18f8d6e
indus <shimizu_s@msi.co.jp>
2019-06-12 19:39:36 +0900

    modified:   init.el

--- init.el
+++ init.el
@@ -753,7 +753,7 @@
   (message ":config google-translate")
   (defvar google-translate-english-chars "[:ascii:]’“”–"
     "When these characters are included, it is considered as English")
-  (defun google-translate-enja-or-jaen (&optional string)
+  (defun google-auto (&optional string)
     "Google translates the region or the current sentence by automatic language detection."
     (interactive)
     (setq string
@@ -778,6 +778,8 @@
        (if asciip "en" "ja")
        (if asciip "ja" "en")
        string)))
+  (define-key evil-normal-state-map (kbd "T") 'google-auto)
+
   )
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
