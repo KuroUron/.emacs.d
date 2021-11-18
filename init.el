903f8fd53e0a94cef0426dd080aeb3ff5a1cb9cb
shimizu_s <shimizu_s@msi.co.jp>
2021-11-18 18:54:36 +0900

    update

--- init.el
+++ init.el
@@ -2116,7 +2116,8 @@ translation it is possible to get suggestion."
       (set-fontset-font "fontset-default"
                         'japanese-jisx0208
                         '("Noto Sans CJK JP Medium" . "iso10646-1")) ; For WSL
-      (set-frame-font "Migu 1M-12:antialias=standard")
+      ;; (set-frame-font "Migu 1M-12:antialias=standard")
+      (set-frame-font "Migu 1M-13:antialias=standard")
       )
 
     ;; Distinguish "C-i" and "TAB"
