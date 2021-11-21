83d56d31acef4a02292d90ec0e2629779f3678ff
benq <shoji>
2021-11-21 10:47:39 +0900

    update

--- init.el
+++ init.el
@@ -698,22 +698,22 @@
   (ivy-rich-mode t)
   )
 
-(use-package ivy-posframe
-  :ensure t
-  :after ivy
-  :config
-  (message ":config ivy-posframe")
+;; (use-package ivy-posframe
+;;   :ensure t
+;;   :after ivy
+;;   :config
+;;   (message ":config ivy-posframe")
 
-  ;; display at `ivy-posframe-style'
-  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display)))
-  ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
-  ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-window-center)))
-  ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-bottom-left)))
-  ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-window-bottom-left)))
-  ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center)))
+;;   ;; display at `ivy-posframe-style'
+;;   ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display)))
+;;   ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
+;;   ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-window-center)))
+;;   ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-bottom-left)))
+;;   (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-window-bottom-left)))
+;;   ;; (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center)))
 
-  (ivy-posframe-mode 1)
-  )
+;;   (ivy-posframe-mode 1)
+;;   )
 
 ;; Enable richer annotations using the Marginalia package
 (use-package marginalia
