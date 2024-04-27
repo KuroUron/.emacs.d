a8e125c35b8d9c1a3d1ee0d35ef8b83194d35376
shimizu_s <shimizu_s@msi.co.jp>
2024-04-27 09:04:22 +0900

    update

--- init.el
+++ init.el
@@ -1620,7 +1620,15 @@ translation it is possible to get suggestion."
 
   (defun my-python-run ()
     (interactive)
-    (let ((command (concat "python -u " (file-name-base) ".py")))
+    (let ((command (concat
+                    ;; "python -u "
+                    (cond
+                     ((string= (system-name) "HARVEST")
+                      "py -u ")
+                     (t
+                      "python -u ")
+                     )
+                    (file-name-base) ".py")))
       (save-buffer)
       (async-shell-command command)
       ))
