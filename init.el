3b4b62a277dc2c013efc59fc28d2d430146cf4e4
shimizu_s <shimizu_s@msi.co.jp>
2025-01-15 20:41:47 +0900

    update

--- init.el
+++ init.el
@@ -249,6 +249,8 @@
                                            1.0) ; for bisco
                                           ((string= (system-name) "aquaman")
                                            1.0) ; for aquaman
+                                          ((string= (system-name) "dullahan")
+                                           1.0) ; for aquaman
                                           (t
                                            0.8)
                                           )
